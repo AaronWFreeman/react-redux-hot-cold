156e3eccad4d24116cb28f6b849d97e1d1500fa1
Aaron Freeman <Freeman0483@gmail.com>
2018-09-05 10:59:08 -0700

    redux-hot-cold

--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,8 @@ import './header.css';
 
 export default function Header(props) {
   return (
-    // 1. renders a Header
-    // 2. Within rendered Header, component renders TopNav
-    // 3. TopNav renders two callback functions
-    // 4. Also renders <h1> tag
     <header>
-      <TopNav
-        onGenerateAuralUpdate={() => props.onGenerateAuralUpdate()}
-        onRestartGame={() => props.onRestartGame()}
-      />
+      <TopNav />
       <h1>HOT or COLD</h1>
     </header>
   );
